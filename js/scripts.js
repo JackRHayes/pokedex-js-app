b0286a23215fd9329eb8a5ee7802c1d07eb283f6
JackRHayes <johnhaze12329@gmail.com>
2023-08-23 18:12:39 -0400

    Created Pokemon list, added three objects to each pokemons arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
-alert('Hello World');
+let pokemonList = [];
+pokemonList = [
+    {
+        name: "Charizard",
+        type: ['fire', 'Flying'],
+        species: 'Lizard',
+        height: 1.7
+    },
+    {
+        name: "Pikachu",
+        type: ['Electric'],
+        species: 'Mouse',
+        height: 0.4
+    },
+    {
+        name: "Squirtle",
+        type: ['Water'],
+        species: 'Turtle',
+        height: 0.5
+    },
 
-let favoriteFood = 'cake';
-document.write(favoriteFood);
+]
