65a489c1fc23c631908d2208cec3749e4c3e0b5e
JackRHayes <johnhaze12329@gmail.com>
2023-09-01 23:28:48 -0400

    Defined the array pokemon list, created special attributes which define what a special pokemon would be (this is defined by the pokemons height), created a for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-let pokemonList = [];
-pokemonList = [
+// Array containing Pokémon objects
+const pokemonList = [
     {
         name: "Charizard",
         type: ['fire', 'Flying'],
@@ -17,6 +17,26 @@ pokemonList = [
         type: ['Water'],
         species: 'Turtle',
         height: 0.5
-    },
+    }
+];
 
-]
+// Define the threshold height for a special Pokémon
+const thresholdHeight = 1.5;
+
+// Iterate over each Pokémon object in the pokemonList array
+for (let i = 0; i < pokemonList.length; i++) {
+    // Get the name and height of the Pokémon
+    const pokemonName = pokemonList[i].name;
+    const pokemonHeight = pokemonList[i].height;
+    
+    // Initialize a variable to store the label
+    let label = '';
+    
+    // Check if the Pokémon's height is above the threshold
+    if (pokemonHeight > thresholdHeight) {
+        label = " - Wow that's a big pokemon!";
+    }
+    
+    // Write the Pokémon name, height, and label to the DOM
+    document.write(`${pokemonName} (height: ${pokemonHeight}${label})<br>`);
+}
