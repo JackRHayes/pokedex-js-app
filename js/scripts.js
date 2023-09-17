85a40b8683f2d07ebfc3065b43967fe4cfd08841
JackRHayes <johnhaze12329@gmail.com>
2023-09-17 13:57:43 -0400

    git add scripts.js
    Replaced for loop with forEach to display Pokémon details

--- js/scripts.js
+++ js/scripts.js
@@ -20,23 +20,17 @@ const pokemonList = [
     }
 ];
 
-// Define the threshold height for a special Pokémon
 const thresholdHeight = 1.5;
 
-// Iterate over each Pokémon object in the pokemonList array
-for (let i = 0; i < pokemonList.length; i++) {
-    // Get the name and height of the Pokémon
-    const pokemonName = pokemonList[i].name;
-    const pokemonHeight = pokemonList[i].height;
-    
-    // Initialize a variable to store the label
+pokemonList.forEach((pokemon) => {
+    const pokemonName = pokemon.name;
+    const pokemonHeight = pokemon.height;
+
     let label = '';
-    
-    // Check if the Pokémon's height is above the threshold
+
     if (pokemonHeight > thresholdHeight) {
         label = " - Wow that's a big pokemon!";
     }
-    
-    // Write the Pokémon name, height, and label to the DOM
+
     document.write(`${pokemonName} (height: ${pokemonHeight}${label})<br>`);
-}
+});
